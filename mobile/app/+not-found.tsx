5b6efa93179b01500511bf3855c8e06557681abc
Alexandre Cathalifaud <alexandre.cathalifaud@epitech.eu>
2024-09-28 22:58:17 +0100

    [UPD] file import

--- mobile/app/+not-found.tsx
+++ mobile/app/+not-found.tsx
@@ -1,8 +1,8 @@
 import { Link, Stack } from "expo-router";
 import { StyleSheet } from "react-native";
 
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+import { ThemedText } from "@/components/ui/ThemedText";
+import { ThemedView } from "@/components/ui/ThemedView";
 
 export default function NotFoundScreen() {
 	return (
