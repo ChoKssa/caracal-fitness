bb826e53bcd89e1dcdbe6c642207cf51aa8f5b17
Alexandre Cathalifaud <alexandre.cathalifaud@epitech.eu>
2024-09-29 12:11:02 +0100

    [ADD] Add AsyncStorageService for storing and retrieving data using AsyncStorage

---
+++ mobile/services/storage/asyncStorage.ts
@@ -0,0 +1,29 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Callback, CallbackWithResult } from '@react-native-async-storage/async-storage/lib/typescript/types';
+
+export const AsyncStorageService = {
+    setItem: async (key: string, value: any, callback?: Callback) => {
+        try {
+            await AsyncStorage.setItem(key, value, callback);
+        } catch (error) {
+            console.error(`AsyncStorage error setItem: ${error}`);
+
+            if (callback) {
+                callback(error as Error | null);
+            }
+        }
+    },
+
+    getItem: async (key: string, callback?: CallbackWithResult<string>): Promise<string | undefined> => {
+        try {
+            return await AsyncStorage.getItem(key, callback) || undefined;
+        } catch (error) {
+            console.error(`AsyncStorage error getItem: ${error}`);
+
+            if (callback) {
+                callback(error as Error | null);
+            }
+
+        }
+    }
+};
