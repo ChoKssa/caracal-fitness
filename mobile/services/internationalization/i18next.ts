c827f6078a46c4876b16fafcaf96d5208c82fd0c
Alexandre Cathalifaud <alexandre.cathalifaud@epitech.eu>
2024-09-29 12:11:14 +0100

    [ADD] i18next configuration for internationalization

---
+++ mobile/services/internationalization/i18next.ts
@@ -0,0 +1,22 @@
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from '@/locales/en.json';
+import fr from '@/locales/fr.json';
+
+export const languageResources = {
+    en: { translation: en },
+    fr: { translation: fr },
+};
+
+i18next.use(initReactI18next).init({
+    supportedLngs: Object.keys(languageResources),
+    compatibilityJSON: 'v3',
+    lng: 'en',
+    fallbackLng: 'en',
+    resources: languageResources,
+    interpolation: {
+        escapeValue: false,
+    },
+});
+
+export default i18next;
