c259ceb460f3946126d53481d39cb91ee6e6a067
Alexandre Cathalifaud <alexandre.cathalifaud@epitech.eu>
2024-10-01 14:18:28 +0100

    [REF] text use LatoRegular font

--- mobile/components/ui/ThemedText.tsx
+++ mobile/components/ui/ThemedText.tsx
@@ -20,7 +20,7 @@ export function ThemedText({
 	return (
 		<Text
 			style={[
-				{ color },
+				{ color, fontFamily: "Lato-Regular" },
 				type === "default" ? styles.default : undefined,
 				type === "title" ? styles.title : undefined,
 				type === "defaultSemiBold" ? styles.defaultSemiBold : undefined,
