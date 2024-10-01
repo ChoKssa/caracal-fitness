ba232179c5abfd1def5d162594a27876188ff0b5
Alexandre Cathalifaud <alexandre.cathalifaud@epitech.eu>
2024-10-01 14:26:45 +0100

    [REF] tabbar component

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,36 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import { Redirect, Tabs } from "expo-router";
+import { Tabs } from "expo-router";
+import { Colors } from "@/constants/Colors";
 
 const TabLayout = () => {
 	return (
-		<>
-			<Tabs
-				screenOptions={{
-					tabBarActiveTintColor: "#FFA001",
-					tabBarInactiveTintColor: "#CDCDE0",
-					tabBarShowLabel: false,
-					tabBarStyle: {
-						backgroundColor: "#161622",
-						borderTopWidth: 1,
-						borderTopColor: "#232533",
-						height: 84,
-					},
+		<Tabs
+			screenOptions={{
+				tabBarActiveTintColor: Colors.common.primary_green,
+				tabBarInactiveTintColor: Colors.common.blue_light,
+				tabBarActiveBackgroundColor: Colors.common.gray_dark,
+				tabBarShowLabel: false,
+				tabBarStyle: {
+					backgroundColor: Colors.common.gray_dark,
+					height: 84,
+				},
+			}}
+		>
+			<Tabs.Screen
+				name="home"
+				options={{
+					title: "Home",
+					headerShown: false,
 				}}
-			>
-				<Tabs.Screen
-					name="home"
-					options={{
-						title: "Home",
-						headerShown: false,
-					}}
-				/>
-			</Tabs>
-
-			<StatusBar
-				backgroundColor="#161622"
-				style="light"
 			/>
-		</>
+		</Tabs>
 	);
 };
 
